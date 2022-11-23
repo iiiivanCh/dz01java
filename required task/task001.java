bd4d01d15b6949eb7c54f8d25d5a00c46f8a5723
iiiivanCh <iiiivanchm@yandex.ru>
2022-11-23 17:12:49 +0300

    add task001

---
+++ required task/task001.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class task001 {
+
+    public static void main(String[] args) {
+        int userNamber = numberUser();
+        System.out.println(factorialFun(userNamber));
+        System.out.println(nthTriangularNumber(userNamber));
+    }
+
+    public static int numberUser() {
+        while (true) {
+            try{
+                System.out.print("Введите натуральное число: ");
+                Scanner iScanner = new Scanner(System.in);
+                int num = iScanner.nextInt();
+                iScanner.close();
+                return num;
+                }
+                catch(Exception ex){                    
+                    System.out.println("Ошибка ввода.");
+                }
+            }
+        }
+        
+    public static double factorialFun(int number) {
+        double result = 1;
+        for (int i = 1; i <= number; i++) result *= i;
+        return result;
+    }
+
+    public static int nthTriangularNumber(int number) {
+        int result = number * (number + 1) / 2;
+        return result;
+    }
+}
