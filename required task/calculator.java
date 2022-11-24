b00e23b9266fcd6aa9c670fc7edcba82cf4f09a2
iiiivanCh <iiiivanchm@yandex.ru>
2022-11-24 16:37:04 +0300

    add task003

---
+++ required task/calculator.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args) {
+        double numberUser01 = getNumberUser();
+        double numberUser02 = getNumberUser();
+        String signUser = getSignUser();
+        double totalUser = getSolution(numberUser01, numberUser02, signUser);
+        System.out.print("Итого: ");
+        System.out.println(totalUser);
+    }
+
+    public static double getNumberUser() {
+        while (true) {
+            try {
+                Scanner input = new Scanner(System.in);
+                System.out.print("Введите число: ");
+                Double num = Double.parseDouble(input.next());
+                // input.close();
+                return num;
+            } catch (Exception ex) {
+                System.out.println("Ошибка ввода");
+            }
+        }
+    }
+
+    public static String getSignUser() {
+        String sign = "";
+        while (true) {
+            System.out.print("Введите знак математического действия: ");
+            Scanner iScanner = new Scanner(System.in);
+            sign = iScanner.nextLine();
+            // iScanner.close();
+            // if ((sign == "*") || (sign == "/") || (sign == "+") || (sign == "-")) {
+            // if (sign == "*") {
+            if (sign.contains("*") || sign.contains("/") || sign.contains("+") || sign.contains("-")) {
+                return sign;
+            } else {
+                System.out.println("Ошибка ввода");
+            }
+        }
+    }
+
+    public static Double getSolution(Double number01, Double number02, String sign) {
+        Double total = 0.0;
+        switch (sign) {
+            case "*":
+                total = number01 * number02;
+                break;
+            case "/":
+                total = number01 / number02;
+                break;
+            case "+":
+                total = number01 + number02;
+                break;
+            case "-":
+                total = number01 - number02;
+                break;
+        }
+        return total;
+    }
+}
